84f3f8c334bcdf2a490383f007359db9bec859c3
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-12-21 13:11:46 +0800

    Remove arg val_x in apply_pca

--- utils.py
+++ utils.py
@@ -41,18 +41,16 @@ def init_loader(batch_size=16, full=False):
     return train_loader, test_loader
 
 
-def apply_pca(dim, train_x, val_x, test_x):
-    """Applys the PCA trained on X of train data on X of train, validation and test data
+def apply_pca(dim, train_x, test_x):
+    """Applies the PCA trained on X of train data on X of train and test data
 
     :param dim: Dimension of the data after PCA
     :param train_x: X of train data of shape B*C*H*W
-    :param val_x: X of validation data of shape B*C*H*W
     :param test_x: X of test data of shape B*C*H*W
-    :return: X of train, validation and test data after PCA
+    :return: X of train and test data after PCA
     """
     # Flatten the data of shape B*C*H*W to B*(C*H*W)
     f_train_x = torch.flatten(train_x, start_dim=1)
-    f_val_x = torch.flatten(val_x, start_dim=1)
     f_test_x = torch.flatten(test_x, start_dim=1)
 
     # Train a PCA with train data
@@ -61,14 +59,16 @@ def apply_pca(dim, train_x, val_x, test_x):
 
     # Transform the data
     new_train_x = pca.transform(f_train_x)
-    new_val_x = pca.transform(f_val_x)
     new_test_x = pca.transform(f_test_x)
 
-    return new_train_x, new_val_x, new_test_x
+    return new_train_x, new_test_x
 
 
-def cross_val():
-    pass
+def cross_val_pca(model, fold):
+    """Cross validation for the model that needs to use PCA processed data
+
+    :return: Precision, recall, f1 values (for each class) and accuracy of the model
+    """
 
 
 if __name__ == '__main__':
